08d4618eca64ed2d24b71d53b5f25ebd168c359f
JoseSanchez54 <55786662+JoseSanchez54@users.noreply.github.com>
2022-07-07 15:07:27 +0200

    rt

---
+++ pages/[categoria].js
@@ -0,0 +1,74 @@
+import axios from "axios";
+import WooCommerce from "../woocommerce/Woocommerce";
+import Nav from "../components/Nav";
+export const getStaticPaths = async () => {
+  const categorias = await WooCommerce.get("products/categories").then(
+    (response) => {
+      return response.data;
+    }
+  );
+
+  const paths = categorias.map((produ) => {
+    return {
+      params: {
+        categoria: produ.slug.toString(),
+      },
+    };
+  });
+  return {
+    paths: paths,
+    fallback: true,
+  };
+};
+export async function getStaticProps(props) {
+  const template = await axios
+    .get(process.env.URLBASE + "/wp-json/jet-cct/ajustes_internos/")
+    .then((res) => res.data[0].plantilla);
+  const internos = await axios
+    .get(process.env.URLBASE + "/wp-json/jet-cct/ajustes_internos/")
+    .then((res) => res.data);
+  const posts = await axios
+    .get(process.env.URLBASE + "wp-json/wp/v2/allposts")
+    .then((res) => res?.data);
+
+  const pagesNew = await axios.get(
+    process.env.URLBASE + "/wp-json/jet-cct/paginas"
+  );
+  const home2 = await pagesNew.data.find(
+    (page) => page.pagina_asociada === "Principal"
+  );
+  const categorias = await WooCommerce.get("products/categories").then(
+    (response) => {
+      return response.data;
+    }
+  );
+  const categoriaActual = categorias.find(
+    (e) => e.slug === props.params.categoria
+  );
+
+  const options = await axios.get(
+    process.env.URLBASE + "/wp-json/jet-cct/opciones_generales/"
+  );
+  const productos = await WooCommerce.get(
+    "products?category=" + categoriaActual.id
+  ).then((response) => {
+    return response.data;
+  });
+
+  return {
+    props: {
+      options: options.data[0],
+      pagesNew: home2,
+      template: template,
+      entradas: posts,
+      internos: internos,
+      productos,
+      categorias,
+    },
+    revalidate: 10,
+  };
+}
+
+export default function Tienda({ options, categorias, pagesNew, vinos }) {
+  return <Nav opciones={options} categorias={categorias} />;
+}
---
+++ pages/tienda.js
@@ -0,0 +1,62 @@
+import axios from "axios";
+
+import WooCommerce from "../woocommerce/Woocommerce";
+
+export default function Tienda({ options, categorias, pagesNew, vinos }) {
+  return (
+    <>
+      <HomeCuervos
+        pagina={pagesNew}
+        categorias={categorias}
+        opciones={options}
+        vinos={vinos}
+      />
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const template = await axios
+    .get(process.env.URLBASE + "/wp-json/jet-cct/ajustes_internos/")
+    .then((res) => res.data[0].plantilla);
+  const internos = await axios
+    .get(process.env.URLBASE + "/wp-json/jet-cct/ajustes_internos/")
+    .then((res) => res.data);
+  const posts = await axios
+    .get(process.env.URLBASE + "wp-json/wp/v2/allposts")
+    .then((res) => res?.data);
+
+  const pagesNew = await axios.get(
+    process.env.URLBASE + "/wp-json/jet-cct/paginas"
+  );
+  const home2 = await pagesNew.data.find(
+    (page) => page.pagina_asociada === "Tienda"
+  );
+
+  const options = await axios.get(
+    process.env.URLBASE + "/wp-json/jet-cct/opciones_generales/"
+  );
+  const categorias = await WooCommerce.get(
+    "products/categories?order=desc"
+  ).then((response) => {
+    return response.data;
+  });
+  const vinos = await WooCommerce.get("products?category=89").then(
+    (response) => {
+      return response.data;
+    }
+  );
+
+  return {
+    props: {
+      options: options.data[0],
+      pagesNew: home2,
+      template: template,
+      entradas: posts,
+      internos: internos,
+      categorias,
+      vinos,
+    },
+    revalidate: 10,
+  };
+}
