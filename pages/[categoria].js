c87cfe48312e926afb0db0fa8b73495bb56da06b
JoseSanchez54 <55786662+JoseSanchez54@users.noreply.github.com>
2022-07-11 10:03:25 +0200

    Revert "Update [categoria].js"
    This reverts commit 15920e8dd4388a0a0b99423a6bfffd85a8f1e5f8.

--- pages/[categoria].js
+++ pages/[categoria].js
@@ -24,6 +24,40 @@ export const getStaticPaths = async () => {
   };
 };
 export async function getStaticProps(props) {
+  const template = await axios
+    .get(process.env.URLBASE + "/wp-json/jet-cct/ajustes_internos/")
+    .then((res) => res.data[0].plantilla);
+  const internos = await axios
+    .get(process.env.URLBASE + "/wp-json/jet-cct/ajustes_internos/")
+    .then((res) => res.data);
+  const posts = await axios
+    .get(process.env.URLBASE + "wp-json/wp/v2/allposts")
+    .then((res) => res?.data);
+
+  const pagesNew = await axios.get(
+    process.env.URLBASE + "/wp-json/jet-cct/paginas"
+  );
+  const home2 = await pagesNew.data.find(
+    (page) => page.pagina_asociada === "tienda"
+  );
+  const categorias = await WooCommerce.get(
+    "products/categories?order=desc"
+  ).then((response) => {
+    return response.data;
+  });
+  const categoriaActual = categorias.find(
+    (e) => e.slug === props.params.categoria
+  );
+
+  const options = await axios.get(
+    process.env.URLBASE + "/wp-json/jet-cct/opciones_generales/"
+  );
+  const productos = await WooCommerce.get(
+    "products?category=" + categoriaActual?.id
+  ).then((response) => {
+    return response.data;
+  });
+
   return {
     props: {
       options: options.data,
