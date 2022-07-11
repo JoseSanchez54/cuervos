feab334a24974ab3e4ef393f05963116493a2488
JoseSanchez54 <55786662+JoseSanchez54@users.noreply.github.com>
2022-07-11 09:42:25 +0200

    rt

--- pages/index.js
+++ pages/index.js
@@ -48,7 +48,7 @@ export async function getStaticProps() {
 
   return {
     props: {
-      options: options.data[0],
+      options: options.data,
       pagesNew: home2,
       template: template,
       entradas: posts,
