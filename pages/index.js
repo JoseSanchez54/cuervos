dbe285aab9729ed7d3bd1c26688acbc94da6d688
JoseSanchez54 <55786662+JoseSanchez54@users.noreply.github.com>
2022-07-11 10:03:32 +0200

    Revert "Update index.js"
    This reverts commit f9bf90146850531a621d17b02345fd477f2bd19f.

--- pages/index.js
+++ pages/index.js
@@ -11,14 +11,41 @@ export default function Home({ options, categorias, pagesNew, vinos }) {
   const { data, isValidating } = usePages(pagesNew, "Principal");
   const { products: productosSWR } = useProducts(vinos); */
 
-  return (
-    <>
-      <h1>TEst</h1>
-    </>
-  );
+  return <></>;
 }
 
 export async function getStaticProps() {
+  const template = await axios
+    .get(process.env.URLBASE + "/wp-json/jet-cct/ajustes_internos/")
+    .then((res) => res.data[0].plantilla);
+  const internos = await axios
+    .get(process.env.URLBASE + "/wp-json/jet-cct/ajustes_internos/")
+    .then((res) => res.data);
+  const posts = await axios
+    .get(process.env.URLBASE + "wp-json/wp/v2/allposts")
+    .then((res) => res?.data);
+
+  const pagesNew = await axios.get(
+    process.env.URLBASE + "/wp-json/jet-cct/paginas"
+  );
+  const home2 = await pagesNew.data.find(
+    (page) => page.pagina_asociada === "Principal"
+  );
+
+  const options = await axios.get(
+    process.env.URLBASE + "/wp-json/jet-cct/opciones_generales/"
+  );
+  const categorias = await WooCommerce.get(
+    "products/categories?order=desc"
+  ).then((response) => {
+    return response.data;
+  });
+  const vinos = await WooCommerce.get("products?category=89").then(
+    (response) => {
+      return response.data;
+    }
+  );
+
   return {
     props: {},
     revalidate: 10,
