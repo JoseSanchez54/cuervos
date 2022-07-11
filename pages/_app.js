9c93dc3c65b9f8a56fba275ae14a08420acaa317
JoseSanchez54 <55786662+JoseSanchez54@users.noreply.github.com>
2022-07-11 10:03:38 +0200

    Revert "Update _app.js"
    This reverts commit c0844c355f323c28bdf8a47a077fcae2561496ab.

--- pages/_app.js
+++ pages/_app.js
@@ -38,7 +38,7 @@ function MyApp({ Component, pageProps }) {
     <SWRConfig
       value={{
         fetcher: fetcher,
-        provider: () => new Map(),
+
         refreshInterval: 30,
       }}
     >
