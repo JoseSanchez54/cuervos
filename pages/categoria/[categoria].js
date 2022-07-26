153ad765b53cb66bc20fc84f37f01c306b86dd31
JoseSanchez54 <55786662+JoseSanchez54@users.noreply.github.com>
2022-07-26 14:50:37 +0200

    Update [categoria].js

--- pages/categoria/[categoria].js
+++ pages/categoria/[categoria].js
@@ -53,7 +53,7 @@ export async function getStaticProps(props) {
     process.env.URLBASE + "/wp-json/jet-cct/opciones_generales/"
   );
   const productos = await WooCommerce.get(
-    "products?category=" + categoriaActual?.id
+    "products?per_page=99&category=" + categoriaActual?.id
   ).then((response) => {
     return response.data;
   });
