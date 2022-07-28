76c4cdc70a722da0bcab4ec550dae2f0706a88e1
JoseSanchez54 <55786662+JoseSanchez54@users.noreply.github.com>
2022-07-28 14:16:38 +0200

    Update Woocommerce.js

--- woocommerce/Woocommerce.js
+++ woocommerce/Woocommerce.js
@@ -1,7 +1,7 @@
 import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
 
 const WooCommerce = new WooCommerceRestApi({
-  url: "https://apicuervos.bitmac.es/",
+  url: process.env.URLBASE,
   consumerKey: "ck_2d7c6d79c2af2edddc60706d02db7bc4d0d219b8",
   consumerSecret: "cs_6a55fc48ffd5e20c18bcb160c2fb9a808108987c",
   version: "wc/v3",
