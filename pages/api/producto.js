4d55f72c85ea8ddeee325e3cffca3b700335a19e
JoseSanchez54 <55786662+JoseSanchez54@users.noreply.github.com>
2022-08-01 10:52:31 +0200

    rt

--- pages/api/producto.js
+++ pages/api/producto.js
@@ -1,7 +1,9 @@
 import WooCommerce from "../../woocommerce/Woocommerce";
 
 export default async (req, res) => {
-  return WooCommerce.get("products" + req.id).then((response) => {
-    return res.status(200).json(response.data);
-  });
+  if (req?.id) {
+    return WooCommerce.get("products" + req?.id).then((response) => {
+      return res.status(200).json(response.data);
+    });
+  }
 };
--- pages/api/variaciones.js
+++ pages/api/variaciones.js
@@ -1,9 +1,11 @@
 import WooCommerce from "../../woocommerce/Woocommerce";
 
 export default async (req, res) => {
-  return WooCommerce.get(
-    "products/" + req.query.id + "/variations" + "?per_page=50"
-  ).then((response) => {
-    return res.status(200).json(response.data);
-  });
+  if (req?.query?.id) {
+    return WooCommerce.get(
+      "products/" + req.query.id + "/variations" + "?per_page=50"
+    ).then((response) => {
+      return res.status(200).json(response.data);
+    });
+  }
 };
