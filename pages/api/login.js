16b9ff5c4bc6519d17ef870ebac73d1bb550fccb
JoseSanchez54 <55786662+JoseSanchez54@users.noreply.github.com>
2022-08-01 12:53:56 +0200

    Update login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -6,6 +6,16 @@ export default async (req, res) => {
     username,
     password,
   };
+  const resWP = await axios
+    .post(
+      process.env.URLBASE +
+        "/wp-json/custom-plugin/login?username=" +
+        username +
+        "&password=" +
+        password
+    )
+    .then((e) => e.data.data);
+  await console.log(resWP);
   const response = await axios
     .post(process.env.URLBASE + "wp-json/api/v1/token", data)
     .then((re) =>
@@ -13,6 +23,9 @@ export default async (req, res) => {
         token: re.data.jwt_token,
         expires: re.data.expires_in,
         iat: re.data.iat,
+        id: resWP.id,
+        username: resWP.user_nicename,
+        email: resWP.user_email,
       })
     );
 };
