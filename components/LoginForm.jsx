e9587d74fc71df90fb9d5f382cb47423b7692d26
JoseSanchez54 <55786662+JoseSanchez54@users.noreply.github.com>
2022-08-01 14:48:36 +0200

    Update LoginForm.jsx

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -8,6 +8,8 @@ import { motion, AnimatePresence } from "framer-motion";
 
 const LoginForm = ({ opciones, login, set }) => {
   const [loading, setLoading] = useState(false);
+  const [olvidar, setOlvidar] = useState(false);
+  const [code, setCode] = useState(false);
   const [error, setError] = useState("");
   const handleActivo = () => {
     set(false);
@@ -15,12 +17,20 @@ const LoginForm = ({ opciones, login, set }) => {
   const [form, setForm] = useState({
     username: "",
     password: "",
+    olvidado: "",
+    code: "",
   });
   const handleForm = (e) => {
     setForm({
       ...form,
       [e.target.name]: e.target.value,
     });
+  };
+  const handleOlvidar = () => {
+    setOlvidar(!olvidar);
+  };
+  const handleCode = () => {
+    setCode(!code);
   };
 
   const SendLogin = async () => {
@@ -43,6 +53,32 @@ const LoginForm = ({ opciones, login, set }) => {
         setError("El login es incorrecto");
         setLoading(false);
       });
+  };
+  const SendOlvidar = async () => {
+    const res = await axios
+      .post(process.env.URLBASE + "wp-json/bdpwr/v1/reset-password", {
+        email: form.olvidado,
+      })
+      .then((res) => {
+        setOlvidar(false);
+        setError("Se ha enviado un correo para restablecer la contraseña");
+      })
+      .catch((err) => setError("El correo no existe"));
+  };
+  const SendCode = () => {
+    setLoading(true);
+    axios
+      .post(process.env.URLBASE + "wp-json/bdpwr/v1/set-password", {
+        code: form.code,
+        email: form.olvidado,
+        password: form.password,
+      })
+      .then((res) => {
+        setCode(false);
+        setOlvidar(false);
+        setError("Se ha restablecido la contraseña");
+      })
+      .catch((err) => setError("El código no es correcto"));
   };
   return (
     <>
@@ -68,88 +104,288 @@ const LoginForm = ({ opciones, login, set }) => {
                 </>
               ) : (
                 <>
-                  <motion.div
-                    key="init"
-                    exit={{
-                      opacity: 0,
-                    }}
-                    animate={{
-                      opacity: 1,
-                    }}
-                    initial={{
-                      opacity: 0,
-                    }}
-                    className="mt-5"
-                  >
-                    <Input
-                      clearable
-                      bordered
-                      labelPlaceholder="Nombre de usuario"
-                      initialValue="Nombre de usuario"
-                      onChange={(e) => handleForm(e)}
-                      name="username"
-                      value={form.username}
-                      required
-                      css={{
-                        backgroundColor: "white",
-                        borderColor: "#7e8085",
-                        label: {
-                          color: "#7e8085",
-                          zIndex: "1",
-                        },
-                        input: {
-                          borderColor: "#7e8085",
-                        },
-                      }}
-                    />
-                  </motion.div>
-                  <motion.div
-                    key="init2"
-                    exit={{
-                      opacity: 0,
-                    }}
-                    animate={{
-                      opacity: 1,
-                    }}
-                    initial={{
-                      opacity: 0,
-                    }}
-                    className="mt-9"
-                  >
-                    <Input
-                      clearable
-                      bordered
-                      labelPlaceholder="Contraseña"
-                      initialValue="password"
-                      onChange={(e) => handleForm(e)}
-                      name="password"
-                      value={form.password}
-                      required
-                      css={{
-                        backgroundColor: "white",
-                        borderColor: "#7e8085",
-                        label: {
-                          color: "#7e8085",
-                          zIndex: "1",
-                        },
-                        input: {
-                          borderColor: "#7e8085",
-                        },
-                      }}
-                    />
-                  </motion.div>
-                  <button
-                    onClick={() => SendLogin()}
-                    className="mt-9"
-                    style={{
-                      backgroundColor: "black",
-                      color: "white",
-                      border: "none",
-                      padding: "10px 20px",
-                    }}
-                  >
-                    Login
-                  </button>
+                  {code && (
+                    <>
+                      <motion.div
+                        key="initee"
+                        exit={{
+                          opacity: 0,
+                        }}
+                        animate={{
+                          opacity: 1,
+                        }}
+                        initial={{
+                          opacity: 0,
+                        }}
+                        className="mt-5"
+                      >
+                        <Input
+                          clearable
+                          bordered
+                          labelPlaceholder="Email"
+                          initialValue="Email"
+                          onChange={(e) => handleForm(e)}
+                          name="username"
+                          value={form.username}
+                          required
+                          css={{
+                            backgroundColor: "white",
+                            borderColor: "#7e8085",
+                            fontFamily: opciones.fuente_global,
+                            label: {
+                              color: "#7e8085",
+                              zIndex: "1",
+                            },
+                            input: {
+                              borderColor: "#7e8085",
+                            },
+                          }}
+                        />
+                      </motion.div>
+                      <motion.div
+                        key="init2"
+                        exit={{
+                          opacity: 0,
+                        }}
+                        animate={{
+                          opacity: 1,
+                        }}
+                        initial={{
+                          opacity: 0,
+                        }}
+                        className="mt-9"
+                      >
+                        <Input
+                          clearable
+                          bordered
+                          labelPlaceholder="Introduce tu nueva contraseña"
+                          initialValue="password"
+                          onChange={(e) => handleForm(e)}
+                          name="password"
+                          value={form.password}
+                          required
+                          css={{
+                            backgroundColor: "white",
+                            borderColor: "#7e8085",
+                            fontFamily: opciones.fuente_global,
+                            label: {
+                              color: "#7e8085",
+                              zIndex: "1",
+                            },
+                            input: {
+                              borderColor: "#7e8085",
+                            },
+                          }}
+                        />
+                      </motion.div>
+                      <motion.div
+                        key="init3"
+                        exit={{
+                          opacity: 0,
+                        }}
+                        animate={{
+                          opacity: 1,
+                        }}
+                        initial={{
+                          opacity: 0,
+                        }}
+                        className="mt-9"
+                      >
+                        <Input
+                          clearable
+                          bordered
+                          labelPlaceholder="Codigo"
+                          initialValue="Codigo"
+                          onChange={(e) => handleForm(e)}
+                          name="code"
+                          value={form.password}
+                          required
+                          css={{
+                            backgroundColor: "white",
+                            borderColor: "#7e8085",
+                            fontFamily: opciones.fuente_global,
+                            label: {
+                              color: "#7e8085",
+                              zIndex: "1",
+                            },
+                            input: {
+                              borderColor: "#7e8085",
+                            },
+                          }}
+                        />
+                      </motion.div>
+                      <button
+                        onClick={() => SendCode()}
+                        className="mt-9"
+                        style={{
+                          backgroundColor: "black",
+                          color: "white",
+                          border: "none",
+                          padding: "10px 20px",
+                        }}
+                      >
+                        Cambiar contraseña
+                      </button>
+                      <button
+                        onClick={() => {
+                          handleOlvidar();
+                          handleCode();
+                        }}
+                      >
+                        Atras
+                      </button>
+                    </>
+                  )}
+                  {olvidar && !code ? (
+                    <>
+                      <motion.div
+                        key="init"
+                        exit={{
+                          opacity: 0,
+                        }}
+                        animate={{
+                          opacity: 1,
+                        }}
+                        initial={{
+                          opacity: 0,
+                        }}
+                        className="mt-5"
+                      >
+                        <Input
+                          clearable
+                          bordered
+                          labelPlaceholder="Email"
+                          initialValue="Email"
+                          onChange={(e) => handleForm(e)}
+                          name="olvidado"
+                          value={form.olvidado}
+                          required
+                          css={{
+                            backgroundColor: "white",
+                            borderColor: "#7e8085",
+                            fontFamily: opciones.fuente_global,
+                            label: {
+                              color: "#7e8085",
+                              zIndex: "1",
+                            },
+                            input: {
+                              borderColor: "#7e8085",
+                            },
+                          }}
+                        />
+                      </motion.div>
+                      <button
+                        onClick={() => SendOlvidar()}
+                        className="mt-9"
+                        style={{
+                          backgroundColor: "black",
+                          color: "white",
+                          border: "none",
+                          padding: "10px 20px",
+                        }}
+                      >
+                        Enviar
+                      </button>
+                      <button onClick={() => handleCode()}>
+                        He recibido el codigo de verificacion
+                      </button>
+                    </>
+                  ) : (
+                    <>
+                      {!code && (
+                        <>
+                          {" "}
+                          <motion.div
+                            key="initd"
+                            exit={{
+                              opacity: 0,
+                            }}
+                            animate={{
+                              opacity: 1,
+                            }}
+                            initial={{
+                              opacity: 0,
+                            }}
+                            className="mt-5"
+                          >
+                            <Input
+                              clearable
+                              bordered
+                              labelPlaceholder="Nombre de usuario"
+                              initialValue="Nombre de usuario"
+                              onChange={(e) => handleForm(e)}
+                              name="username"
+                              value={form.username}
+                              required
+                              css={{
+                                backgroundColor: "white",
+                                borderColor: "#7e8085",
+                                fontFamily: opciones.fuente_global,
+                                label: {
+                                  color: "#7e8085",
+                                  zIndex: "1",
+                                },
+                                input: {
+                                  borderColor: "#7e8085",
+                                },
+                              }}
+                            />
+                          </motion.div>
+                          <motion.div
+                            key="init4"
+                            exit={{
+                              opacity: 0,
+                            }}
+                            animate={{
+                              opacity: 1,
+                            }}
+                            initial={{
+                              opacity: 0,
+                            }}
+                            className="mt-9"
+                          >
+                            <Input
+                              clearable
+                              bordered
+                              labelPlaceholder="Contraseña"
+                              initialValue="password"
+                              onChange={(e) => handleForm(e)}
+                              name="password"
+                              value={form.password}
+                              required
+                              css={{
+                                backgroundColor: "white",
+                                borderColor: "#7e8085",
+                                fontFamily: opciones.fuente_global,
+                                label: {
+                                  color: "#7e8085",
+                                  zIndex: "1",
+                                },
+                                input: {
+                                  borderColor: "#7e8085",
+                                },
+                              }}
+                            />
+                          </motion.div>
+                          <button
+                            onClick={() => SendLogin()}
+                            className="mt-9"
+                            style={{
+                              backgroundColor: "black",
+                              color: "white",
+                              border: "none",
+                              padding: "10px 20px",
+                            }}
+                          >
+                            Login
+                          </button>
+                          <button onClick={() => handleOlvidar()}>
+                            He olvidado mi contraseña
+                          </button>
+                        </>
+                      )}
+                    </>
+                  )}
                 </>
               )}
               {!loading && (
