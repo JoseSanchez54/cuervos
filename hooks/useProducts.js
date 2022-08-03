3c22aadb89f0137444653b8c235ba6d133afdd97
JoseSanchez54 <55786662+JoseSanchez54@users.noreply.github.com>
2022-08-03 16:26:11 +0200

    Update useProducts.js

--- hooks/useProducts.js
+++ hooks/useProducts.js
@@ -1,9 +1,11 @@
 import useSWR from "swr";
 
 export const useProducts = (initialdata, categoria) => {
-  const { data, error, isValidating, mutate } = useSWR("/api/test", {
+  const fetcher = (url) => fetch(url).then((r) => r.json());
+  const { data, error, isValidating, mutate } = useSWR("/api/test", fetcher, {
     fallbackData: initialdata,
     categoria: categoria,
+    refreshInterval: 1000,
   });
 
   return {
