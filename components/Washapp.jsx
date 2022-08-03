3e207b26696e60e937e914e71cdcb73c54a333ae
JoseSanchez54 <55786662+JoseSanchez54@users.noreply.github.com>
2022-08-03 11:51:44 +0200

    Update Washapp.jsx

--- components/Washapp.jsx
+++ components/Washapp.jsx
@@ -8,6 +8,7 @@ const Washapp = () => {
       <button className="fixed bottom-2 right-2">
         <a
           target="_blank"
+          rel="noreferrer"
           href={"https://api.whatsapp.com/send?phone=" + options?.washapp}
         >
           <Image height="60px" width="60px" src="/whatsapp.svg"></Image>
