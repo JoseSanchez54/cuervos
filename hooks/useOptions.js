be86caea8c7301ce189eb8e49606e4e1faf31759
JoseSanchez54 <55786662+JoseSanchez54@users.noreply.github.com>
2022-08-04 11:32:55 +0200

    Update useOptions.js

--- hooks/useOptions.js
+++ hooks/useOptions.js
@@ -2,8 +2,10 @@ import useSWR from "swr";
 import { useMemo } from "react";
 
 export const useOptions = (initialdata) => {
+  const fetcher = (url) => fetch(url).then((r) => r.json());
   const { data, isValidating, error } = useSWR(
     process.env.URLBASE + "wp-json/jet-cct/opciones_generales",
+    fetcher,
     {
       fallbackData: [initialdata],
     }
