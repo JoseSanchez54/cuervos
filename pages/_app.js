efcf2e01c3f19f91d8c2c67b7411b8059da57a70
JoseSanchez54 <55786662+JoseSanchez54@users.noreply.github.com>
2022-08-04 12:40:46 +0200

    rt

--- pages/_app.js
+++ pages/_app.js
@@ -10,8 +10,10 @@ import { useStore } from "react-redux";
 import { useDispatch } from "react-redux";
 import axios from "axios";
 import Washapp from "../components/Washapp";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
+  
   const redirectURL =
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
@@ -43,6 +45,15 @@ function MyApp({ Component, pageProps }) {
     >
       <PersistGate persistor={store.__persistor}>
         <NextUIProvider>
+          <Script id="google-tag-manager" strategy="afterInteractive">
+            {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${process.env.GTM_ID}');
+      `}
+          </Script>
           <Component {...pageProps} />
           <Washapp />
         </NextUIProvider>
