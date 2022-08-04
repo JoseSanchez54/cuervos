50a68fa2d771184b4cac77b7550310e5f74091b5
JoseSanchez54 <55786662+JoseSanchez54@users.noreply.github.com>
2022-08-04 11:10:09 +0200

    Update test.js

--- pages/api/test.js
+++ pages/api/test.js
@@ -2,7 +2,7 @@ import WooCommerce from "../../woocommerce/Woocommerce";
 
 export default async (req, res) => {
   if (req?.categoria?.id) {
-    return WooCommerce.get(
+    await WooCommerce.get(
       "products?per_page=50products?category=" + req?.categoria?.id
     ).then((response) => {
       return res.status(200).json(response.data);
