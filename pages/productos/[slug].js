ee43369102cdb2164b3337396e942d5438de140f
JoseSanchez54 <55786662+JoseSanchez54@users.noreply.github.com>
2022-08-09 09:35:32 +0200

    Update [slug].js

--- pages/productos/[slug].js
+++ pages/productos/[slug].js
@@ -376,7 +376,7 @@ const SingleProduct = ({
                       className={
                         variations.data.length > 3
                           ? "flex lg:flex-row  gap-5 w-full p-5 lg:flex-nowrap flex-wrap justify-center"
-                          : "flex lg:flex-row-reverse flex-row-reverse gap-5 w-full p-5 lg:flex-nowrap flex-wrap justify-center"
+                          : "flex flex-row  gap-5 w-full p-5 lg:flex-nowrap flex-wrap justify-center"
                       }
                     >
                       {variations.data.map((e, index) => {
