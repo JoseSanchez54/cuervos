25b58363b20231665f854f0b035b4ce3ef4fafe9
JoseSanchez54 <55786662+JoseSanchez54@users.noreply.github.com>
2022-08-10 08:32:59 +0200

    Update mi-cuenta.js

--- pages/mi-cuenta.js
+++ pages/mi-cuenta.js
@@ -103,8 +103,8 @@ export default function MiCuenta({
           </div>
         </div>
       </div>
-      <div className="flex flex-row my-9 w-full lg:min-h-[76vh]  justify-center ">
-        <div className="flex flex-col w-full max-w-[1600px]  items-center">
+      <div className="flex flex-row w-full justify-center">
+        <div className="flex flex-col items-center max-w-[1600px]">
           <div className="flex flex-row justify-end w-full my-5">
             <button
               onClick={() => handleConnect()}
@@ -120,170 +120,193 @@ export default function MiCuenta({
               Desconectarse
             </button>
           </div>
-          <span className="titulo my- uppercase">Pedidos</span>
-          <div className="grid w-full lg:grid-cols-6 grid-cols-4 gap-4 ">
-            <div className="lg:block hidden text-center">
-              <span className="encabezado">ID</span>
-            </div>
-            <div className="text-center">
-              <span className="encabezado">Total</span>
-            </div>
-            <div className="text-center">
-              <span className="encabezado">Estado</span>
-            </div>
-            <div className="text-center">
-              <span className="encabezado">Calle</span>
-            </div>
-            <div className="text-center">
-              <span className="encabezado">Ciudad</span>
-            </div>
-            <div className="text-center lg:block hidden">
-              <span className="encabezado">CP</span>
-            </div>
-          </div>
-          {pedidos1.isValidating ? (
-            <div className="my-9 h-full pt-9 justify-center flex w-full">
-              <SyncLoader />
-            </div>
-          ) : (
-            <>
-              {userOrders?.map((order, index) => {
-                return (
-                  <div
-                    key={index}
-                    className={
-                      index % 2 === 0
-                        ? "grid w-full lg:grid-cols-6 grid-cols-4 gap-4 py-5"
-                        : "grid w-full lg:grid-cols-6 grid-cols-4 gap-4 py-5 bg-[#f7f7f7]"
-                    }
-                  >
-                    <div className=" lg:block hidden text-center">
-                      <span className="dato">{order?.id}</span>
+          <div className="flex flex-row w-full gap-9 justify-center">
+            <div className="flex flex-col w-full lg:w-1/2 items-center">
+              <div className="flex flex-row my-9 w-full lg:min-h-[76vh]  justify-center ">
+                <div className="flex flex-col w-full max-w-[1600px]  items-center">
+                  <span className="titulo my-9 uppercase">Pedidos</span>
+                  <div className="grid w-full lg:grid-cols-6 grid-cols-4 gap-4 ">
+                    <div className="lg:block hidden text-center">
+                      <span className="encabezado">ID</span>
                     </div>
                     <div className="text-center">
-                      <span className="dato">{order?.total}€</span>
+                      <span className="encabezado">Total</span>
                     </div>
                     <div className="text-center">
-                      <span
-                        className={
-                          (order?.status === "processing" &&
-                            "dato processing") ||
-                          (order?.status === "completed" && "dato completed") ||
-                          (order?.status === "cancelled" && "dato cancelled") ||
-                          (order?.status === "pending" && "dato pending")
-                        }
-                      >
-                        {order?.status}
-                      </span>
+                      <span className="encabezado">Estado</span>
                     </div>
                     <div className="text-center">
-                      <span className="dato">{order?.billing?.address_1}</span>
+                      <span className="encabezado">Calle</span>
                     </div>
                     <div className="text-center">
-                      <span className="dato">{order?.billing?.city}</span>
+                      <span className="encabezado">Ciudad</span>
                     </div>
                     <div className="text-center lg:block hidden">
-                      <span className="dato">{order?.billing?.postcode}</span>
+                      <span className="encabezado">CP</span>
                     </div>
                   </div>
-                );
-              })}
-            </>
-          )}
-        </div>
-      </div>
-      <div className="flex flex-row my-9 w-full lg:min-h-[76vh]  justify-center ">
-        <div className="flex flex-col w-full max-w-[1600px]  items-center">
-          <span className="titulo my-6 uppercase">Suscripciones</span>
-          <div className="grid w-full lg:grid-cols-6 grid-cols-4 gap-4 ">
-            <div className="lg:block hidden text-center">
-              <span className="encabezado">ID</span>
+                  {pedidos1.isValidating ? (
+                    <div className="my-9 h-full pt-9 justify-center flex w-full">
+                      <SyncLoader />
+                    </div>
+                  ) : (
+                    <>
+                      {userOrders?.map((order, index) => {
+                        return (
+                          <div
+                            key={index}
+                            className={
+                              index % 2 === 0
+                                ? "grid w-full lg:grid-cols-6 grid-cols-4 gap-4 py-5"
+                                : "grid w-full lg:grid-cols-6 grid-cols-4 gap-4 py-5 bg-[#f7f7f7]"
+                            }
+                          >
+                            <div className=" lg:block hidden text-center">
+                              <span className="dato">{order?.id}</span>
+                            </div>
+                            <div className="text-center">
+                              <span className="dato">{order?.total}€</span>
+                            </div>
+                            <div className="text-center">
+                              <span
+                                className={
+                                  (order?.status === "processing" &&
+                                    "dato processing") ||
+                                  (order?.status === "completed" &&
+                                    "dato completed") ||
+                                  (order?.status === "cancelled" &&
+                                    "dato cancelled") ||
+                                  (order?.status === "pending" &&
+                                    "dato pending")
+                                }
+                              >
+                                {order?.status}
+                              </span>
+                            </div>
+                            <div className="text-center">
+                              <span className="dato">
+                                {order?.billing?.address_1}
+                              </span>
+                            </div>
+                            <div className="text-center">
+                              <span className="dato">
+                                {order?.billing?.city}
+                              </span>
+                            </div>
+                            <div className="text-center lg:block hidden">
+                              <span className="dato">
+                                {order?.billing?.postcode}
+                              </span>
+                            </div>
+                          </div>
+                        );
+                      })}
+                    </>
+                  )}
+                </div>
+              </div>
             </div>
-            <div className="text-center">
-              <span className="encabezado">Total</span>
-            </div>
-            <div className="text-center">
-              <span className="encabezado">Estado</span>
-            </div>
-            <div className="text-center">
-              <span className="encabezado">Siguiente pago</span>
-            </div>
-            <div className="text-center">
-              <span className="encabezado">Último pago</span>
-            </div>
-            <div className="text-center lg:block hidden">
-              <span className="encabezado">CP</span>
-            </div>
-          </div>
-          {pedidos1.isValidating ? (
-            <div className="my-9 h-full pt-9 justify-center flex w-full">
-              <SyncLoader />
-            </div>
-          ) : (
-            <>
-              {userSus?.map((order, index) => {
-                return (
-                  <div
-                    key={index}
-                    className={
-                      index % 2 === 0
-                        ? "grid w-full lg:grid-cols-6 grid-cols-4 gap-4 py-5"
-                        : "grid w-full lg:grid-cols-6 grid-cols-4 gap-4 py-5 bg-[#f7f7f7]"
-                    }
-                  >
-                    <div className=" lg:block hidden text-center">
-                      <span className="dato">{order?.id}</span>
+            <div className="flex flex-col w-full lg:w-1/2 items-center">
+              <div className="flex flex-row my-9 w-full lg:min-h-[76vh]  justify-center ">
+                <div className="flex flex-col w-full max-w-[1600px]  items-center">
+                  <span className="titulo my-9 uppercase">Suscripciones</span>
+                  <div className="grid w-full lg:grid-cols-6 grid-cols-4 gap-4 ">
+                    <div className="lg:block hidden text-center">
+                      <span className="encabezado">ID</span>
                     </div>
                     <div className="text-center">
-                      <span className="dato">{order?.total}€</span>
+                      <span className="encabezado">Total</span>
                     </div>
                     <div className="text-center">
-                      <span
-                        className={
-                          (order?.status === "processing" &&
-                            "dato processing") ||
-                          (order?.status === "processing" &&
-                            "dato completed") ||
-                          (order?.status === "cancelled" && "dato cancelled") ||
-                          (order?.status === "on-hold" && "dato pending")
-                        }
-                      >
-                        {order?.status}
-                      </span>
+                      <span className="encabezado">Estado</span>
                     </div>
                     <div className="text-center">
-                      <span className="dato">
-                        {new Date(
-                          order?.next_payment_date_gmt
-                        )?.toLocaleDateString("es-ES", {
-                          weekday: "long",
-                          year: "numeric",
-                          month: "short",
-                          day: "numeric",
-                        })}
-                      </span>
+                      <span className="encabezado">Siguiente pago</span>
                     </div>
                     <div className="text-center">
-                      <span className="dato">
-                        {" "}
-                        {new Date(
-                          order?.last_payment_date_gmt
-                        )?.toLocaleDateString("es-ES", {
-                          weekday: "long",
-                          year: "numeric",
-                          month: "short",
-                          day: "numeric",
-                        })}
-                      </span>
+                      <span className="encabezado">Último pago</span>
                     </div>
                     <div className="text-center lg:block hidden">
-                      <span className="dato">{order?.billing?.postcode}</span>
+                      <span className="encabezado">CP</span>
                     </div>
                   </div>
-                );
-              })}
-            </>
-          )}
+                  {pedidos1.isValidating ? (
+                    <div className="my-9 h-full pt-9 justify-center flex w-full">
+                      <SyncLoader />
+                    </div>
+                  ) : (
+                    <>
+                      {userSus?.map((order, index) => {
+                        return (
+                          <div
+                            key={index}
+                            className={
+                              index % 2 === 0
+                                ? "grid w-full lg:grid-cols-6 grid-cols-4 gap-4 py-5"
+                                : "grid w-full lg:grid-cols-6 grid-cols-4 gap-4 py-5 bg-[#f7f7f7]"
+                            }
+                          >
+                            <div className=" lg:block hidden text-center">
+                              <span className="dato">{order?.id}</span>
+                            </div>
+                            <div className="text-center">
+                              <span className="dato">{order?.total}€</span>
+                            </div>
+                            <div className="text-center">
+                              <span
+                                className={
+                                  (order?.status === "processing" &&
+                                    "dato processing") ||
+                                  (order?.status === "processing" &&
+                                    "dato completed") ||
+                                  (order?.status === "cancelled" &&
+                                    "dato cancelled") ||
+                                  (order?.status === "on-hold" &&
+                                    "dato pending")
+                                }
+                              >
+                                {order?.status}
+                              </span>
+                            </div>
+                            <div className="text-center">
+                              <span className="dato">
+                                {new Date(
+                                  order?.next_payment_date_gmt
+                                )?.toLocaleDateString("es-ES", {
+                                  weekday: "long",
+                                  year: "numeric",
+                                  month: "short",
+                                  day: "numeric",
+                                })}
+                              </span>
+                            </div>
+                            <div className="text-center">
+                              <span className="dato">
+                                {" "}
+                                {new Date(
+                                  order?.last_payment_date_gmt
+                                )?.toLocaleDateString("es-ES", {
+                                  weekday: "long",
+                                  year: "numeric",
+                                  month: "short",
+                                  day: "numeric",
+                                })}
+                              </span>
+                            </div>
+                            <div className="text-center lg:block hidden">
+                              <span className="dato">
+                                {order?.billing?.postcode}
+                              </span>
+                            </div>
+                          </div>
+                        );
+                      })}
+                    </>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
