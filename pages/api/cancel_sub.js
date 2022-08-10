17944d5d5d14e60f3f460521f4627e83735a41f7
JoseSanchez54 <55786662+JoseSanchez54@users.noreply.github.com>
2022-08-10 14:34:12 +0200

    Update cancel_sub.js

--- pages/api/cancel_sub.js
+++ pages/api/cancel_sub.js
@@ -1,5 +1,5 @@
 import Stripe from "stripe";
-
+import WooCommerce from "../../woocommerce/Woocommerce";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async (req, res) => {
