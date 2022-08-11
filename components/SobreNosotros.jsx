dc76323b57a013f8f09af17cb11175064732e627
JoseSanchez54 <55786662+JoseSanchez54@users.noreply.github.com>
2022-08-11 11:16:46 +0200

    Update SobreNosotros.jsx

--- components/SobreNosotros.jsx
+++ components/SobreNosotros.jsx
@@ -8,21 +8,21 @@ const SobreNosotros = ({ opciones, pagina, categorias }) => {
   return (
     <>
       <DefaultSeo
-        title={pagina.titulo_pagina}
-        description={pagina.descripcion_de_pagina}
+        title={pagina?.titulo_pagina}
+        description={pagina?.descripcion_de_pagina}
         canonical={process.env.URLFINAL + "/sobre-nosotros"}
         additionalLinkTags={[
           {
             rel: "icon",
-            href: opciones.favicon_principal,
+            href: opciones?.favicon_principal,
           },
         ]}
         openGraph={{
           type: "website",
           locale: "en_ES",
           url: process.env.URLFINAL,
-          site_name: opciones.nombre_sitio,
-          description: opciones.descripcion_sitio,
+          site_name: opciones?.nombre_sitio,
+          description: opciones?.descripcion_sitio,
         }}
         twitter={{
           handle: "@handle",
@@ -40,19 +40,19 @@ const SobreNosotros = ({ opciones, pagina, categorias }) => {
             <div className="flex flex-col justify-center items-center w-full h-full">
               <div className="p-9 z-[10] flex flex-col justify-center items-center">
                 <span className="z-[10] uppercase text-center tituloPrimera ">
-                  {pagina.pretitulo_nosotros}
+                  {pagina?.pretitulo_nosotros}
                 </span>
                 <span className="z-[10] uppercase text-center titulo ">
-                  {pagina.titulo_nosotros}
+                  {pagina?.titulo_nosotros}
                 </span>
                 <p className="uppercase parrafoPrimera max-w-[350px] text-center">
-                  {pagina.parrafo_nosotros}
+                  {pagina?.parrafo_nosotros}
                 </p>
               </div>
 
               <Image
                 objectFit="cover"
-                src={pagina.primera_imagen_nosotros}
+                src={pagina?.primera_imagen_nosotros}
                 layout="fill"
                 priority="high"
                 quality={100}
@@ -69,10 +69,10 @@ const SobreNosotros = ({ opciones, pagina, categorias }) => {
           <div className="flex flex-row flex-wrap  my-[50px] w-full ">
             <div className="flex flex-col items-center w-full">
               <span className="z-[10] uppercase text-center tituloPrimera ">
-                {pagina.segunda_primer_titulo_nosotros}
+                {pagina?.segunda_primer_titulo_nosotros}
               </span>
               <p className="uppercase parrafoPrimera mt-[20px]  max-w-[350px] text-center">
-                {pagina.segunda_primer_parrafo_nosotros}
+                {pagina?.segunda_primer_parrafo_nosotros}
               </p>
             </div>
           </div>
@@ -81,38 +81,38 @@ const SobreNosotros = ({ opciones, pagina, categorias }) => {
               <Image
                 width="658px"
                 height="747px"
-                src={pagina.imagen_tercera_nosotros}
+                src={pagina?.imagen_tercera_nosotros}
               />
             </div>
             <div className="flex flex-col lg:w-1/2 w-full justify-center">
               <span className="z-[10] uppercase text-center mb-9 tituloPrimera ">
-                {pagina.titulo_tercera_nosotros}
+                {pagina?.titulo_tercera_nosotros}
               </span>
               <p className="uppercase parrafoPrimera  text-center">
-                {pagina.parrafo_tercera_nosotros}
+                {pagina?.parrafo_tercera_nosotros}
               </p>
             </div>
           </div>
           <div className="flex gap-9 flex-row my-[50px] lg:flex-nowrap w-full justify-center flex-wrap-reverse">
             <div className="flex flex-col lg:w-1/2 w-full justify-center">
               <span className="z-[10] uppercase text-center mb-9 tituloPrimera ">
-                {pagina.titulo_cuarta_nosotros}
+                {pagina?.titulo_cuarta_nosotros}
               </span>
               <p className="uppercase parrafoPrimera  text-center">
-                {pagina.parrafo_cuarta_nosotros}
+                {pagina?.parrafo_cuarta_nosotros}
               </p>
             </div>
             <div className="flex flex-col lg:w-1/2 w-full">
               <Image
                 width="658px"
                 height="747px"
-                src={pagina.imagen_cuarta_nosotros}
+                src={pagina?.imagen_cuarta_nosotros}
               />
             </div>
           </div>
           <div className="flex flex-row w-full justify-center">
             <div className="relative w-full min-h-[587px]">
-              <Image layout="fill" src={pagina.imagen_quinta_nosotros} />
+              <Image layout="fill" src={pagina?.imagen_quinta_nosotros} />
             </div>
           </div>
           <div className="flex gap-9 flex-row my-[50px] flex-wrap-reverse lg:flex-nowrap w-full justify-center">
@@ -120,15 +120,15 @@ const SobreNosotros = ({ opciones, pagina, categorias }) => {
               <Image
                 width="658px"
                 height="747px"
-                src={pagina.imagen_sexta_nosotros}
+                src={pagina?.imagen_sexta_nosotros}
               />
             </div>
             <div className="flex flex-col lg:w-1/2 w-full justify-center">
               <span className="z-[10] uppercase text-center mb-9 tituloPrimera ">
-                {pagina.titulo_sexta_nosotros}
+                {pagina?.titulo_sexta_nosotros}
               </span>
               <p className="uppercase parrafoPrimera  text-center">
-                {pagina.parrafo_sexta_nosotros}
+                {pagina?.parrafo_sexta_nosotros}
               </p>
             </div>
           </div>
@@ -138,27 +138,27 @@ const SobreNosotros = ({ opciones, pagina, categorias }) => {
       <Footer options={opciones}></Footer>
       <style jsx>{`
         .titulo {
-          font-family: ${opciones.fuente_titulos};
+          font-family: ${opciones?.fuente_titulos};
           font-size: 120px;
         }
         .tituloPrimera {
-          font-family: ${opciones.fuente_titulos};
+          font-family: ${opciones?.fuente_titulos};
           font-size: 38px;
         }
         .parrafoPrimera {
-          font-family: ${opciones.fuente_global};
+          font-family: ${opciones?.fuente_global};
           font-size: 16px;
         }
         .alto {
           height: calc(100vh - 144px);
         }
         .tituloseccion {
-          font-family: ${opciones.fuente_titulos};
+          font-family: ${opciones?.fuente_titulos};
           font-size: 36px;
         }
         @media (max-width: 1022px) {
           .titulo {
-            font-family: ${opciones.fuente_titulos};
+            font-family: ${opciones?.fuente_titulos};
             font-size: 90px;
             line-height: 1.1;
           }
