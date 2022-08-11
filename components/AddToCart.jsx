7bd7a330a68964a9053ea4ff65f6a9dbcf345215
JoseSanchez54 <55786662+JoseSanchez54@users.noreply.github.com>
2022-08-11 11:10:38 +0200

    Update AddToCart.jsx

--- components/AddToCart.jsx
+++ components/AddToCart.jsx
@@ -9,17 +9,19 @@ const AddToCart = ({ seleccion, lista, producto, opciones, precio }) => {
 
   const actualCart = useSelector((state) => state.cartReducer.cart);
   useEffect(() => {
-    actualCart.map((item) => {
+    actualCart.map((item, index) => {
       const periodo = item?.meta_data?.find(
         (meta) => meta?.key === "_subscription_period"
       )?.value;
       const intervalo = item?.meta_data?.find(
         (meta) => meta?.key === "_subscription_period_interval"
       )?.value;
-      setSus({
-        periodo,
-        intervalo,
-      });
+      if (index === 0) {
+        setSus({
+          periodo,
+          intervalo,
+        });
+      }
     });
   }, [actualCart]);
   const variable = producto.attributes.length > 0;
@@ -43,14 +45,14 @@ const AddToCart = ({ seleccion, lista, producto, opciones, precio }) => {
     if (variable) {
       console.log(sus);
       const productoAdd = addToCart(seleccion, lista);
-      productoAdd = {
-        ...productoAdd,
-        nombrePadre: producto.name,
-        idPadre: producto.id,
-        variable: true,
-        img: producto.images[0].src,
-      };
+
       if (producto.type === "variable-subscription") {
+        productoAdd = {
+          ...productoAdd,
+          nombrePadre: producto.name,
+          idPadre: producto.id,
+          variable: true,
+        };
         const periodo = productoAdd?.meta_data?.find(
           (meta) => meta?.key === "_subscription_period"
         )?.value;
