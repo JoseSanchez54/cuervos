1bbbf91754e830371c06f32912a18255a7a39e06
JoseSanchez54 <55786662+JoseSanchez54@users.noreply.github.com>
2022-08-16 12:18:40 +0200

    Update SingleGrid.jsx

--- components/SingleGrid.jsx
+++ components/SingleGrid.jsx
@@ -62,129 +62,123 @@ const SingleGrid = ({ producto, opciones }) => {
   )[0]?.value;
 
   return (
-    <AnimatePresence>
-      <div
-        key={producto?.id}
-        style={{ cursor: "pointer" }}
-        onMouseOver={(e) => setCambioImagen(true)}
-        onMouseLeave={(e) => setCambioImagen(false)}
-        className="flex flex-col z-[21] w-full h-[508px] max-h-[508px]  max-w-[404px]"
+    <div
+      key={producto?.id}
+      style={{ cursor: "pointer" }}
+      onMouseOver={(e) => setCambioImagen(true)}
+      onMouseLeave={(e) => setCambioImagen(false)}
+      className="flex flex-col z-[21] w-full h-[508px] max-h-[508px]  max-w-[404px]"
+    >
+      <Link
+        href={{
+          pathname: "/productos/[slug]",
+          query: { slug: producto?.slug },
+        }}
       >
-        <Link
-          href={{
-            pathname: "/productos/[slug]",
-            query: { slug: producto?.slug },
-          }}
-        >
-          <a className="h-full">
-            <>
-              <div className="relative w-full max-h-[508px] h-full max-w-[404px]">
-                <div className="flex flex-col p-7">
-                  <Link
-                    href={{
-                      pathname: "/productos/[slug]",
-                      query: { slug: producto?.slug },
-                    }}
-                  >
-                    <a className="z-[20] mb-3">
-                      <span
-                        style={{
-                          color: cambioImagen ? "#fff" : "#000",
-                          fontFamily: opciones.fuente_global,
-                          textTransform: "uppercase",
-                          fontWeight: "bold",
-                        }}
-                        className="z-[10]"
-                      >
-                        {producto?.name}
-                      </span>
-                    </a>
-                  </Link>
-                  {producto?.categories[0]?.slug === "destacados" ? (
-                    <Precio
-                      precio={
-                        precioindividual !== ""
-                          ? precioindividual
-                          : producto?.regular_price
-                      }
-                      rebaja={
-                        precioindividualrebaja
-                          ? precioindividualrebaja
-                          : producto?.sale_price
-                      }
-                      hover={cambioImagen}
-                      opciones={opciones}
-                      variable={producto?.type === "variable"}
-                      variaciones={variacion}
-                      cat="destacados"
-                    />
-                  ) : (
-                    <Precio
-                      precio={producto?.regular_price}
-                      rebaja={producto?.sale_price}
-                      hover={cambioImagen}
-                      opciones={opciones}
-                      variable={producto?.type === "variable"}
-                      variaciones={variacion}
-                    />
-                  )}
+        <a className="h-full">
+          <>
+            <div className="relative w-full max-h-[508px] h-full max-w-[404px]">
+              <div className="flex flex-col p-7">
+                <Link
+                  href={{
+                    pathname: "/productos/[slug]",
+                    query: { slug: producto?.slug },
+                  }}
+                >
+                  <a className="z-[20] mb-3">
+                    <span
+                      style={{
+                        color: cambioImagen ? "#fff" : "#000",
+                        fontFamily: opciones.fuente_global,
+                        textTransform: "uppercase",
+                        fontWeight: "bold",
+                      }}
+                      className="z-[10]"
+                    >
+                      {producto?.name}
+                    </span>
+                  </a>
+                </Link>
+                {producto?.categories[0]?.slug === "destacados" ? (
+                  <Precio
+                    precio={
+                      precioindividual !== ""
+                        ? precioindividual
+                        : producto?.regular_price
+                    }
+                    rebaja={
+                      precioindividualrebaja
+                        ? precioindividualrebaja
+                        : producto?.sale_price
+                    }
+                    hover={cambioImagen}
+                    opciones={opciones}
+                    variable={producto?.type === "variable"}
+                    variaciones={variacion}
+                    cat="destacados"
+                  />
+                ) : (
+                  <Precio
+                    precio={producto?.regular_price}
+                    rebaja={producto?.sale_price}
+                    hover={cambioImagen}
+                    opciones={opciones}
+                    variable={producto?.type === "variable"}
+                    variaciones={variacion}
+                  />
+                )}
 
-                  <Link
+                <Link
+                  variants={texto}
+                  initial="initial"
+                  whileHover="hover"
+                  href={{
+                    pathname: "/productos/[slug]",
+                    query: { slug: producto?.slug },
+                  }}
+                  passHref
+                >
+                  <motion.a
                     variants={texto}
                     initial="initial"
                     whileHover="hover"
-                    href={{
-                      pathname: "/productos/[slug]",
-                      query: { slug: producto?.slug },
-                    }}
-                    passHref
                   >
-                    <motion.a
-                      variants={texto}
-                      initial="initial"
-                      whileHover="hover"
+                    <button
+                      style={{
+                        fontFamily: opciones.fuente_global,
+                        zIndex: "10",
+                        fontSize: "12px",
+                        color: cambioImagen ? "#fff" : "#000",
+                        display: "flex",
+                        border: cambioImagen
+                          ? "1px solid #fff"
+                          : "1px solid #000",
+                        width: "fit-content",
+                        padding: "10px 20px",
+                        marginTop: "10px",
+                        "&:hover": {
+                          backgroundColor: "#000",
+                          color: "#fff",
+                        },
+                      }}
                     >
-                      <button
-                        style={{
-                          fontFamily: opciones.fuente_global,
-                          zIndex: "10",
-                          fontSize: "12px",
-                          color: cambioImagen ? "#fff" : "#000",
-                          display: "flex",
-                          border: cambioImagen
-                            ? "1px solid #fff"
-                            : "1px solid #000",
-                          width: "fit-content",
-                          padding: "10px 20px",
-                          marginTop: "10px",
-                          "&:hover": {
-                            backgroundColor: "#000",
-                            color: "#fff",
-                          },
-                        }}
-                      >
-                        COMPRAR
-                      </button>
-                    </motion.a>
-                  </Link>
-                </div>
-                {hover && (
-                  <Image
-                    objectFit="cover"
-                    layout="fill"
-                    src={!cambioImagen ? producto?.images[0].src : hover}
-                  ></Image>
-                )}
+                      COMPRAR
+                    </button>
+                  </motion.a>
+                </Link>
               </div>
-            </>
-          </a>
-        </Link>
-      </div>
-      <style jsx>{`
-        @media (max-width: 1022px) {
-        }
-      `}</style>
-    </AnimatePresence>
+              {hover && (
+                <Image
+                  objectFit="cover"
+                  layout="fill"
+                  src={!cambioImagen ? producto?.images[0].src : hover}
+                ></Image>
+              )}
+            </div>
+          </>
+        </a>
+      </Link>
+    </div>
   );
 };
 export default SingleGrid;
