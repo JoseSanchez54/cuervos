e9e3a692e8f592949fae3a56ed78177721e5e5ae
JoseSanchez54 <55786662+JoseSanchez54@users.noreply.github.com>
2022-08-16 13:05:46 +0200

    Update UserForm.jsx

--- components/UserForm.jsx
+++ components/UserForm.jsx
@@ -70,232 +70,229 @@ const UserForm = ({ opciones, usuario }) => {
         <>
           <div className="flex fixed top-0 right-0 left-0 bottom-0 flex-row w-screen h-screen justify-center items-center">
             <div className="flex max-w-[700px] gap-5 py-9 px-9 bg-white flex-col w-full items-center">
-              <div className="flex mb-9 w-full flex-row justify-between">
-                <span
-                  style={{
-                    fontSize: "1.5rem",
-                    fontWeight: "bold",
-                    color: "#000",
-                    fontFamily: opciones.fuente_global,
-                  }}
-                >
-                  Editar Perfil
-                </span>
-                <button onClick={() => handleActivo()}>
-                  <GrClose size="15px" />
-                </button>
-              </div>
+              <form onSubmit={(e) => handleEnviar(e)}>
+                <div className="flex mb-9 w-full flex-row justify-between">
+                  <span
+                    style={{
+                      fontSize: "1.5rem",
+                      fontWeight: "bold",
+                      color: "#000",
+                      fontFamily: opciones.fuente_global,
+                    }}
+                  >
+                    Editar Perfil
+                  </span>
+                  <button onClick={() => handleActivo()}>
+                    <GrClose size="15px" />
+                  </button>
+                </div>
 
-              <div className="flex my-8 flex-row gap-5 w-full">
-                <div className="flex flex-col w-1/2">
-                  <Input
-                    onChange={(e) => handleForm(e)}
-                    clearable
-                    labelPlaceholder="Nombre"
-                    name="nombre"
-                    initialValue={usuario.nombre}
-                    required
-                    css={{
-                      backgroundColor: "white",
-                      borderColor: "#7e8085",
-                      fontFamily: opciones.fuente_global,
-                      borderRadius: "0",
-                      label: {
-                        color: "#7e8085",
-                        zIndex: "1",
-                      },
-                      input: {
+                <div className="flex my-8 flex-row gap-5 w-full">
+                  <div className="flex flex-col w-1/2">
+                    <Input
+                      onChange={(e) => handleForm(e)}
+                      clearable
+                      labelPlaceholder="Nombre"
+                      name="nombre"
+                      initialValue={usuario.nombre}
+                      required
+                      css={{
+                        backgroundColor: "white",
                         borderColor: "#7e8085",
+                        fontFamily: opciones.fuente_global,
                         borderRadius: "0",
-                      },
-                    }}
-                  />
-                </div>
-                <div className="flex flex-col w-1/2">
-                  {" "}
-                  <Input
-                    onChange={(e) => handleForm(e)}
-                    clearable
-                    labelPlaceholder="Apellido"
-                    name="apellido"
-                    initialValue={usuario.apellido}
-                    required
-                    css={{
-                      backgroundColor: "white",
-                      borderColor: "#7e8085",
-                      fontFamily: opciones.fuente_global,
-                      borderRadius: "0",
-                      label: {
-                        color: "#7e8085",
-                        zIndex: "1",
-                      },
-                      input: {
+                        label: {
+                          color: "#7e8085",
+                          zIndex: "1",
+                        },
+                        input: {
+                          borderColor: "#7e8085",
+                          borderRadius: "0",
+                        },
+                      }}
+                    />
+                  </div>
+                  <div className="flex flex-col w-1/2">
+                    {" "}
+                    <Input
+                      onChange={(e) => handleForm(e)}
+                      clearable
+                      labelPlaceholder="Apellido"
+                      name="apellido"
+                      initialValue={usuario.apellido}
+                      required
+                      css={{
+                        backgroundColor: "white",
                         borderColor: "#7e8085",
+                        fontFamily: opciones.fuente_global,
                         borderRadius: "0",
-                      },
-                    }}
-                  />
+                        label: {
+                          color: "#7e8085",
+                          zIndex: "1",
+                        },
+                        input: {
+                          borderColor: "#7e8085",
+                          borderRadius: "0",
+                        },
+                      }}
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className="flex my-8 flex-row gap-5 w-full">
-                <div className="flex flex-col w-1/2">
-                  <Input
-                    onChange={(e) => handleForm(e)}
-                    clearable
-                    labelPlaceholder="Email"
-                    name="email"
-                    initialValue={usuario.email}
-                    required
-                    css={{
-                      backgroundColor: "white",
-                      borderColor: "#7e8085",
-                      fontFamily: opciones.fuente_global,
-                      borderRadius: "0",
-                      label: {
-                        color: "#7e8085",
-                        zIndex: "1",
-                      },
-                      input: {
+                <div className="flex my-8 flex-row gap-5 w-full">
+                  <div className="flex flex-col w-1/2">
+                    <Input
+                      onChange={(e) => handleForm(e)}
+                      clearable
+                      labelPlaceholder="Email"
+                      name="email"
+                      initialValue={usuario.email}
+                      required
+                      css={{
+                        backgroundColor: "white",
                         borderColor: "#7e8085",
+                        fontFamily: opciones.fuente_global,
                         borderRadius: "0",
-                      },
-                    }}
-                  />
-                </div>
-                <div className="flex flex-col w-1/2">
-                  {" "}
-                  <Input
-                    onChange={(e) => handleForm(e)}
-                    clearable
-                    labelPlaceholder="Teléfono"
-                    name="telefono"
-                    initialValue={usuario.telefono}
-                    required
-                    css={{
-                      backgroundColor: "white",
-                      borderColor: "#7e8085",
-                      fontFamily: opciones.fuente_global,
-                      borderRadius: "0",
-                      label: {
-                        color: "#7e8085",
-                        zIndex: "1",
-                      },
-                      input: {
+                        label: {
+                          color: "#7e8085",
+                          zIndex: "1",
+                        },
+                        input: {
+                          borderColor: "#7e8085",
+                          borderRadius: "0",
+                        },
+                      }}
+                    />
+                  </div>
+                  <div className="flex flex-col w-1/2">
+                    {" "}
+                    <Input
+                      onChange={(e) => handleForm(e)}
+                      clearable
+                      labelPlaceholder="Teléfono"
+                      name="telefono"
+                      initialValue={usuario.telefono}
+                      required
+                      css={{
+                        backgroundColor: "white",
                         borderColor: "#7e8085",
+                        fontFamily: opciones.fuente_global,
                         borderRadius: "0",
-                      },
-                    }}
-                  />
+                        label: {
+                          color: "#7e8085",
+                          zIndex: "1",
+                        },
+                        input: {
+                          borderColor: "#7e8085",
+                          borderRadius: "0",
+                        },
+                      }}
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className="flex my-8 flex-row gap-5 w-full">
-                <div className="flex flex-col w-1/2">
-                  <Input
-                    onChange={(e) => handleForm(e)}
-                    clearable
-                    labelPlaceholder="Dirección"
-                    name="direccion"
-                    initialValue={usuario.direccion}
-                    required
-                    css={{
-                      backgroundColor: "white",
-                      borderColor: "#7e8085",
-                      fontFamily: opciones.fuente_global,
-                      borderRadius: "0",
-                      label: {
-                        color: "#7e8085",
-                        zIndex: "1",
-                      },
-                      input: {
+                <div className="flex my-8 flex-row gap-5 w-full">
+                  <div className="flex flex-col w-1/2">
+                    <Input
+                      onChange={(e) => handleForm(e)}
+                      clearable
+                      labelPlaceholder="Dirección"
+                      name="direccion"
+                      initialValue={usuario.direccion}
+                      required
+                      css={{
+                        backgroundColor: "white",
                         borderColor: "#7e8085",
+                        fontFamily: opciones.fuente_global,
                         borderRadius: "0",
-                      },
-                    }}
-                  />
-                </div>
-                <div className="flex flex-col w-1/2">
-                  <Input
-                    onChange={(e) => handleForm(e)}
-                    clearable
-                    labelPlaceholder="Código postal"
-                    name="cp"
-                    initialValue={usuario.codigoPostal}
-                    required
-                    css={{
-                      backgroundColor: "white",
-                      borderColor: "#7e8085",
-                      fontFamily: opciones.fuente_global,
-                      borderRadius: "0",
-                      label: {
-                        color: "#7e8085",
-                        zIndex: "1",
-                      },
-                      input: {
+                        label: {
+                          color: "#7e8085",
+                          zIndex: "1",
+                        },
+                        input: {
+                          borderColor: "#7e8085",
+                          borderRadius: "0",
+                        },
+                      }}
+                    />
+                  </div>
+                  <div className="flex flex-col w-1/2">
+                    <Input
+                      onChange={(e) => handleForm(e)}
+                      clearable
+                      labelPlaceholder="Código postal"
+                      name="cp"
+                      initialValue={usuario.codigoPostal}
+                      required
+                      css={{
+                        backgroundColor: "white",
                         borderColor: "#7e8085",
+                        fontFamily: opciones.fuente_global,
                         borderRadius: "0",
-                      },
-                    }}
-                  />
+                        label: {
+                          color: "#7e8085",
+                          zIndex: "1",
+                        },
+                        input: {
+                          borderColor: "#7e8085",
+                          borderRadius: "0",
+                        },
+                      }}
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className="flex my-8 flex-row gap-5 w-full">
-                <div className="flex flex-col w-1/2">
-                  <Input
-                    onChange={(e) => handleForm(e)}
-                    clearable
-                    labelPlaceholder="País"
-                    name="pais"
-                    initialValue={usuario.pais}
-                    required
-                    css={{
-                      backgroundColor: "white",
-                      borderColor: "#7e8085",
-                      fontFamily: opciones.fuente_global,
-                      borderRadius: "0",
-                      label: {
-                        color: "#7e8085",
-                        zIndex: "1",
-                      },
-                      input: {
+                <div className="flex my-8 flex-row gap-5 w-full">
+                  <div className="flex flex-col w-1/2">
+                    <Input
+                      onChange={(e) => handleForm(e)}
+                      clearable
+                      labelPlaceholder="País"
+                      name="pais"
+                      initialValue={usuario.pais}
+                      required
+                      css={{
+                        backgroundColor: "white",
                         borderColor: "#7e8085",
+                        fontFamily: opciones.fuente_global,
                         borderRadius: "0",
-                      },
-                    }}
-                  />
-                </div>
-                <div className="flex flex-col w-1/2">
-                  <Input
-                    onChange={(e) => handleForm(e)}
-                    clearable
-                    labelPlaceholder="Ciudad"
-                    name="ciudad"
-                    initialValue={usuario.ciudad}
-                    required
-                    css={{
-                      backgroundColor: "white",
-                      borderColor: "#7e8085",
-                      fontFamily: opciones.fuente_global,
-                      borderRadius: "0",
-                      label: {
-                        color: "#7e8085",
-                        zIndex: "1",
-                      },
-                      input: {
+                        label: {
+                          color: "#7e8085",
+                          zIndex: "1",
+                        },
+                        input: {
+                          borderColor: "#7e8085",
+                          borderRadius: "0",
+                        },
+                      }}
+                    />
+                  </div>
+                  <div className="flex flex-col w-1/2">
+                    <Input
+                      onChange={(e) => handleForm(e)}
+                      clearable
+                      labelPlaceholder="Ciudad"
+                      name="ciudad"
+                      initialValue={usuario.ciudad}
+                      required
+                      css={{
+                        backgroundColor: "white",
                         borderColor: "#7e8085",
+                        fontFamily: opciones.fuente_global,
                         borderRadius: "0",
-                      },
-                    }}
-                  />
+                        label: {
+                          color: "#7e8085",
+                          zIndex: "1",
+                        },
+                        input: {
+                          borderColor: "#7e8085",
+                          borderRadius: "0",
+                        },
+                      }}
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className="flex flex-row w-full justify-center">
-                <button
-                  onClick={(e) => handleEnviar(e)}
-                  className="botoncuenta"
-                >
-                  Guardar
-                </button>
-              </div>
+                <div className="flex flex-row w-full justify-center">
+                  <button className="botoncuenta">Guardar</button>
+                </div>
+              </form>
             </div>
           </div>
         </>
@@ -315,6 +312,7 @@ const UserForm = ({ opciones, usuario }) => {
           color: black;
           border: 1px solid black;
           font-family: ${opciones.fuente_global};
+          text-transform: uppercase;
         }
         button.botoncuenta:hover {
           background-color: black;
