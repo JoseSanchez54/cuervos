7751313b569c6696d749dfee580de49fd8a2e90d
JoseSanchez54 <55786662+JoseSanchez54@users.noreply.github.com>
2022-08-23 11:20:44 +0200

    Update LoginForm.jsx

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -647,6 +647,8 @@ const LoginForm = ({ opciones, login, set }) => {
                                 borderColor: "#7e8085",
                                 fontFamily: opciones.fuente_global,
                                 borderRadius: "0",
+                                maxWidth: "144px",
+                                width: "100%",
                                 label: {
                                   color: "#7e8085",
                                   zIndex: "1",
@@ -658,8 +660,6 @@ const LoginForm = ({ opciones, login, set }) => {
                                 },
                                 div: {
                                   borderRadius: "0",
-                                  maxWidth: "144px",
-                                  width: "100%",
                                 },
                               }}
                             />
