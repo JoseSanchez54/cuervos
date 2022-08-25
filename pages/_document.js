b98d7c367a1de13ff5578742c3d8be551bb04e6c
JoseSanchez54 <55786662+JoseSanchez54@users.noreply.github.com>
2022-08-25 08:11:49 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,10 @@ export default function Document() {
           src="https://cdn.onesignal.com/sdks/OneSignalSDK.js"
           async=""
         ></script>
-        <script src="https://www.paypal.com/sdk/js?client-id=AVbAI_7_F8U_6sR6QBDOWXY2bOpPfgbNFsBYn8wkAvNngJUPmADuObbPEgIa8qayu09XfAEPKInRTyZm&currency=EUR"></script>
+        <script
+          async=""
+          src="https://www.paypal.com/sdk/js?client-id=AVbAI_7_F8U_6sR6QBDOWXY2bOpPfgbNFsBYn8wkAvNngJUPmADuObbPEgIa8qayu09XfAEPKInRTyZm&currency=EUR"
+        ></script>
       </Head>
       <body>
         <Main />
