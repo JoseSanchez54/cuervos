86e423312577dfd6a163a0c42d11c20ba4e79c5a
JoseSanchez54 <55786662+JoseSanchez54@users.noreply.github.com>
2022-09-05 17:34:23 +0200

    Delete fbpixel.js

--- utils/fbpixel.js
+++
@@ -1,8 +0,0 @@
-export const pageview = () => {
-  window.fbq("track", "PageView");
-};
-
-// https://developers.facebook.com/docs/facebook-pixel/advanced/
-export const event = (name, options = {}) => {
-  window.fbq("track", name, options);
-};
