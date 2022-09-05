e75c3910f15059bc967e45370e7ab6426049f3ed
JoseSanchez54 <55786662+JoseSanchez54@users.noreply.github.com>
2022-09-05 16:56:27 +0200

    rt

--- pages/_document.js
+++ pages/_document.js
@@ -17,6 +17,14 @@ export default function Document() {
             __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;" />`,
           }}
         />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style="display:none"
+            src="https://www.facebook.com/tr?id=397822372456827&ev=PageView&noscript=1"
+          />
+        </noscript>
       </body>
     </Html>
   );
