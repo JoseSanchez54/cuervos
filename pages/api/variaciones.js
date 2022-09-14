fb05deff52da67333a0a3bbf7b8e4208b9cc63b0
JoseSanchez54 <55786662+JoseSanchez54@users.noreply.github.com>
2022-09-14 17:27:44 +0200

    Update variaciones.js

--- pages/api/variaciones.js
+++ pages/api/variaciones.js
@@ -1,11 +1,14 @@
 import WooCommerce from "../../woocommerce/Woocommerce";
 
 export default async (req, res) => {
-  if (req?.query?.id) {
-    await WooCommerce.get(
+  if (req.query.id) {
+    const variaciones = await WooCommerce.get(
       "products/" + req.query.id + "/variations" + "?per_page=50"
     ).then((response) => {
-      return res.status(200).json(response.data);
+      return response.data;
     });
+    await res.status(200).json(variaciones);
+  } else {
+    return;
   }
 };
