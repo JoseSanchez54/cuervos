af59ac9e82aa0ad0153897b2e9868e1759efb282
JoseSanchez54 <55786662+JoseSanchez54@users.noreply.github.com>
2022-09-14 18:04:52 +0200

    rt

--- hooks/useProduct.js
+++ hooks/useProduct.js
@@ -1,10 +1,11 @@
 import useSWR from "swr";
+import fetcherWc from "../utils/fetcherWc";
 
 export const useProduct = (initialdata, id) => {
   const fetcher = (url) => fetch(url).then((r) => r.json());
   const { data, error, isValidating, mutate } = useSWR(
-    "/api/producto",
-    fetcher,
+    "products/" + id,
+    fetcherWc,
     {
       fallbackData: initialdata,
       id: id,
--- pages/api/producto.js
+++
@@ -1,8 +0,0 @@
-import WooCommerce from "../../woocommerce/Woocommerce";
-
-export default async (req, res) => {
-  const x = await WooCommerce.get("products/" + req?.id).then((response) => {
-    return response.data;
-  });
-  await res.status(200).json(x);
-};
