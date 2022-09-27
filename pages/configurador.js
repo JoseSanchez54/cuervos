8c281c4316cf9d8f32c6747304da6cfc6c6220e3
JoseSanchez54 <55786662+JoseSanchez54@users.noreply.github.com>
2022-09-27 17:07:35 +0200

    Update configurador.js

--- pages/configurador.js
+++ pages/configurador.js
@@ -8,7 +8,9 @@ const Footer = dynamic(() => import("../components/Footer"), { ssr: false });
 const Grid = dynamic(() => import("../components/Grid"), { ssr: false });
 import { useOptions } from "../hooks/useOptions";
 import { useSelector, useDispatch } from "react-redux";
-
+const DefaultSeo = dynamic(() =>
+  import("next-seo").then((mod) => mod.DefaultSeo)
+);
 export async function getStaticProps() {
   const options = await axios.get(
     process.env.URLBASE + "/wp-json/jet-cct/opciones_generales/"
@@ -88,6 +90,31 @@ const Configurador = ({ options, categorias, productos }) => {
   };
   return (
     <>
+      <DefaultSeo
+        title={"Cría Cuervos - Configurador"}
+        description={
+          "Pagina de configuración de producto de Vinos Cría Cuervos"
+        }
+        canonical={process.env.URLFINAL}
+        additionalLinkTags={[
+          {
+            rel: "icon",
+            href: options.favicon_principal,
+          },
+        ]}
+        openGraph={{
+          type: "website",
+          locale: "en_ES",
+          url: process.env.URLFINAL,
+          site_name: options.nombre_sitio,
+          description: options.descripcion_sitio,
+        }}
+        twitter={{
+          handle: "@handle",
+          site: "@site",
+          cardType: "summary_large_image",
+        }}
+      />
       <Nav categorias={categorias} opciones={optionsSWR} />
       <div className="flex flex-row lg:min-h-[600px] w-full justify-center my-9">
         <div className="flex flex-col max-w-[1200px] items-center w-full">
