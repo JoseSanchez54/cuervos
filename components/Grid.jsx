18dc956b4254352142508384960de1cd3151b082
JoseSanchez54 <55786662+JoseSanchez54@users.noreply.github.com>
2022-09-30 13:43:57 +0200

    Revert "rt"
    This reverts commit b6587d6170b1a79c3edf3e11f2f35f7850cd01f8.

--- components/Grid.jsx
+++ components/Grid.jsx
@@ -1,41 +1,40 @@
 import dynamic from "next/dynamic";
-const SingleGrid = dynamic(() => import("./SingleGrid"));
-import useMobile from "../hooks/useMobile";
-const OfertaSlider = dynamic(() => import("./OfertaSlider"));
-const Grid = ({ productos, opciones, max = 1000 }) => {
-  const { isMobile } = useMobile();
+const SingleGrid = dynamic(() => import("../components/SingleGrid"));
+const SingleGridConfig = dynamic(() =>
+  import("../components/SingleGridConfig")
+);
+const Grid = ({ productos, opciones, max = 1000, config, botellas }) => {
   return (
     <>
-      {isMobile ? (
-        <>
-          <div className="contents">
-            <OfertaSlider categorias={productos} opciones={opciones} />
-          </div>
-        </>
-      ) : (
-        <>
-          {productos.map((producto, index) => {
-            return (
+      {productos.map((producto, index) => {
+        return (
+          <>
+            {index < max && (
               <>
-                {index < max && (
-                  <>
-                    <div
-                      className="flex flex-col gap-2 z-[21] w-full   max-w-[232px]"
-                      key={producto.id}
-                    >
-                      <SingleGrid
-                        producto={producto}
-                        opciones={opciones}
-                        oferta={true}
-                      />
-                    </div>
-                  </>
+                {config === true ? (
+                  <SingleGridConfig
+                    producto={producto}
+                    opciones={opciones}
+                    botellas={botellas}
+                  />
+                ) : (
+                  <div
+                    className="flex flex-col gap-2 z-[21] w-full h-[548px] max-h-[548px]  max-w-[232px]"
+                    key={producto.id}
+                  >
+                    <SingleGrid
+                      producto={producto}
+                      opciones={opciones}
+                      config={config}
+                      oferta={true}
+                    />
+                  </div>
                 )}
               </>
-            );
-          })}
-        </>
-      )}
+            )}
+          </>
+        );
+      })}
     </>
   );
 };
