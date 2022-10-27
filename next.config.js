915b5be82c00eaab9592ca1a32fdc38ce84ddfdd
JoseSanchez54 <55786662+JoseSanchez54@users.noreply.github.com>
2022-10-27 09:35:43 +0200

    next 13.0

--- next.config.js
+++ next.config.js
@@ -35,11 +35,7 @@ module.exports = {
   },
   reactStrictMode: false,
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-      },
-    ],
+    remotePatterns: [],
     domains: ["apicuervos.bitmac.es", "criacuervos.bitmac.es"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
