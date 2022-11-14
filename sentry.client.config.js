43de95b85bbca00f9291dcc6b988c1675959cb19
JoseSanchez54 <55786662+JoseSanchez54@users.noreply.github.com>
2022-11-14 11:31:16 +0100

    Update sentry.client.config.js

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -2,12 +2,12 @@
 // The config you add here will be used whenever a page is visited.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: "https://3bf81d2179694014a509bc468cd7ebf3@o1183910.ingest.sentry.io/4504156833710080",
+  dsn: SENTRY_DSN || 'https://3bf81d2179694014a509bc468cd7ebf3@o1183910.ingest.sentry.io/4504156833710080',
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
